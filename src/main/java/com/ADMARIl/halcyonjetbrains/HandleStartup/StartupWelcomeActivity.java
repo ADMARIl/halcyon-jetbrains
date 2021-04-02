6a36871b39593dcbc7be7793a9d32e4cafcd3232
Andrew Ingson <aings1@umbc.edu>
2021-04-02 03:06:13 -0400

    Implemented forcing of editor colors after first theme run

--- src/main/java/com/ADMARIl/halcyonjetbrains/HandleStartup/StartupWelcomeActivity.java
+++ src/main/java/com/ADMARIl/halcyonjetbrains/HandleStartup/StartupWelcomeActivity.java
@@ -1,6 +1,8 @@
 package com.ADMARIl.halcyonjetbrains.HandleStartup;
 
 import com.intellij.notification.*;
+import com.intellij.openapi.editor.colors.EditorColorsManager;
+import com.intellij.openapi.editor.colors.EditorColorsScheme;
 import com.intellij.openapi.options.ShowSettingsUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
@@ -15,15 +17,11 @@ public class StartupWelcomeActivity implements StartupActivity {
             return;
         }
 
-//        NotificationGroup notificationGroup = new NotificationGroup(
-//                "Halcyon Theme", NotificationDisplayType.STICKY_BALLOON, true
-//        );
-
         NotificationListener.Adapter notificationListener = new NotificationListener.Adapter() {
             @Override
             protected void hyperlinkActivated(@NotNull Notification notification, @NotNull HyperlinkEvent e) {
                 if (!project.isDisposed()) {
-                    ShowSettingsUtil.getInstance().showSettingsDialog(project, "Appearance & Behavior");
+                    ShowSettingsUtil.getInstance().showSettingsDialog(project, "Appearance");
                 }
             }
         };
@@ -31,10 +29,15 @@ public class StartupWelcomeActivity implements StartupActivity {
         Notification notification = NotificationGroupManager.getInstance().getNotificationGroup("Halcyon Theme").createNotification(
                 "Halcyon-Theme",
                 "If you don't see the correct theme, make sure it's selected in " +
-                        "<a href=\"#\">Settings → Appearance & Behavior → Appearance → Theme</a>",
+                        "<a href=\"#\">Settings > Appearance & Behavior > Appearance > Theme</a>",
                 NotificationType.INFORMATION,
                 notificationListener
         );
+
+        // Hopefully forces theme if using for the first time
+        EditorColorsManager manager = EditorColorsManager.getInstance();
+        EditorColorsScheme scheme = manager.getScheme("Halcyon");
+        EditorColorsManager.getInstance().setGlobalScheme(scheme);
 
         Notifications.Bus.notify(notification);
         HalcyonSettings.getInstance().setWelcomeNotifyShowed(true);
